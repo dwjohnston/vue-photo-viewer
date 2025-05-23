84ee39c2a5fe5949b4cfac4199b04ae8004704b0
David Johnston <david@blacksheepcode.com>
2025-05-24 09:20:54 +1000

    Basic search input returning results upto PhotoExplorer workign

--- src/components/PhotoExplorer.vue
+++ src/components/PhotoExplorer.vue
@@ -1,14 +1,27 @@
 <script setup lang="ts">
+import { ref, onMounted } from 'vue';
 import PhotoDisplay from './PhotoDisplay.vue';
-import PhotoSearchInput from './PhotoSearchInput.vue';
+import PhotoSearchInput, { type PhotoSearchResponse } from './PhotoSearchInput.vue';
 
+// Reactive variable to store the search query
+const searchResult = ref<PhotoSearchResponse | null>(null)
+const data = ref<string | null>(null);
+
+const updateSearchQuery = (query: PhotoSearchResponse | null) => {
+    searchResult.value = query;
+};
 
 </script>
 
 <template>
-    <div>photo explorer</div>
-    <PhotoSearchInput />
-    <PhotoDisplay />
+    <div>
+        <div>photo explorer</div>
+        <!-- Pass the updateSearchQuery method to PhotoSearchInput -->
+        <PhotoSearchInput @updateSearchQuery="updateSearchQuery" />
+        <PhotoDisplay />
+        <div v-if="data">{{ data }}</div>
+        <div v-if="searchResult">Search Query: {{ searchResult }}</div>
+    </div>
 </template>
 
 <style scoped></style>
